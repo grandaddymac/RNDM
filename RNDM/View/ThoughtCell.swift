37f779739d38c763a7175fd48f53edf28a22fd0b
gdm <gdm.mbp@gdmMBP.local>
2018-12-22 11:09:13 -0500

    Setup MainVC and configure tableViewCell

---
+++ RNDM/View/ThoughtCell.swift
@@ -0,0 +1,32 @@
+//
+//  ThoughtCell.swift
+//  RNDM
+//
+//  Created by gdm on 12/22/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import UIKit
+
+class ThoughtCell: UITableViewCell {
+
+    //Outlets
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var timestampLbl: UILabel!
+    @IBOutlet weak var thoughtTxtLbl: UILabel!
+    @IBOutlet weak var likesImage: UIImageView!
+    @IBOutlet weak var likesNumLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func configureCell(thought: Thought) {
+        
+    }
+
+   
+
+}
