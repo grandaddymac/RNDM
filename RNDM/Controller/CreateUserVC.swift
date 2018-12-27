30600165c5f7fc0c43c042a16af8732202308f06
gdm <gdm.mbp@gdmMBP.local>
2018-12-26 20:21:14 -0500

    Setup Firebase authentication

--- RNDM/Controller/CreateUserVC.swift
+++ RNDM/Controller/CreateUserVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class CreateUserVC: UIViewController {
 
@@ -28,8 +30,38 @@ class CreateUserVC: UIViewController {
     }
     
     @IBAction func createTapped(_ sender: Any) {
+        
+        guard let email = emailText.text,
+            let password = passwordText.text,
+            let username = usernameText.text else { return }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            if let error = error {
+                debugPrint("Error creating user: \(error.localizedDescription)")
+            }
+            
+            let changeRequest = user?.user.createProfileChangeRequest()
+            changeRequest?.displayName = username
+            changeRequest?.commitChanges(completion: { (error) in
+                if let error = error {
+                    debugPrint(error.localizedDescription)
+                }
+            })
+            guard let userId = user?.user.uid else { return }
+            Firestore.firestore().collection(USERS_REF).document(userId).setData([
+                USERNAME : username,
+                DATE_CREATED : FieldValue.serverTimestamp()
+                ], completion: { (error) in
+                    if let error = error {
+                        debugPrint(error.localizedDescription)
+                    } else {
+                        self.dismiss(animated: true, completion: nil)
+                    }
+            })
+        }
     }
     @IBAction func cancelTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     
 
--- RNDM/Utilities/Constants.swift
+++ RNDM/Utilities/Constants.swift
@@ -10,9 +10,13 @@ import Foundation
 
 
 let THOUGHTS_REF = "thoughts"
+let USERS_REF = "users"
+
 let CATEGORY = "category"
 let NUM_COMMENTS = "numComments"
 let NUM_LIKES = "numLikes"
 let THOUGHT_TXT = "thoughtTxt"
 let TIMESTAMP = "timestamp"
 let USERNAME = "username"
+let DATE_CREATED = "dateCreated"
+
